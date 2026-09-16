a3709e4e8dd075fc04374c0060b112c69dc4adff
PAULAMI0511 <paula.rodriguez-g@mail.escuelaing.edu.co>
2026-09-16 17:03:35 -0500

    Completada Actividad 4: Creacion del robot diferencial en URDF/Xacro y visualizacion en RViz

---
+++ src/dif_bot_description/setup.py
@@ -0,0 +1,27 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'dif_bot_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='paula01',
+    maintainer_email='paula01@todo.todo',
+    description='Robot diferencial URDF Xacro',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [],
+    },
+)
