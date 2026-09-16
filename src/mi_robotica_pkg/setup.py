e58105701271802ba54a21d60334e8accde43e54
PAULAMI0511 <paula.rodriguez-g@mail.escuelaing.edu.co>
2026-09-16 10:34:23 -0500

    Actividad 1 lista: turtle_mimic.launch.py

---
+++ src/mi_robotica_pkg/setup.py
@@ -0,0 +1,27 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'mi_robotica_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='paula01',
+    maintainer_email='paula.rodriguez-g@mail.escuelaing.edu.co',
+    description='Paquete para actividad de launch files y mimic',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [],
+    },
+)
