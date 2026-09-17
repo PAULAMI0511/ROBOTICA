d589da478f68d849949fc20437044c6bc48bf564
PAULAMI0511 <paula.rodriguez-g@mail.escuelaing.edu.co>
2026-09-16 19:43:07 -0500

    Completada Actividad 7: Configuracion de display_launch.py y urdf_config.rviz

---
+++ src/dif_bot_description/launch/display_launch.py
@@ -0,0 +1,39 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import xacro
+
+def generate_launch_description():
+    pkg_path = get_package_share_directory('dif_bot_description')
+    xacro_file = os.path.join(pkg_path, 'urdf', 'dif_bot_description.urdf.xacro')
+    rviz_config_file = os.path.join(pkg_path, 'urdf_config.rviz')
+
+    robot_description_raw = xacro.process_file(xacro_file).toxml()
+
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description_raw}]
+    )
+
+    node_joint_state_publisher_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen'
+    )
+
+    node_rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config_file]
+    )
+
+    return LaunchDescription([
+        node_robot_state_publisher,
+        node_joint_state_publisher_gui,
+        node_rviz
+    ])
--- src/dif_bot_description/setup.py
+++ src/dif_bot_description/setup.py
@@ -9,19 +9,23 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro')),
+        # Incluir archivos launch y rviz
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name), glob('*.rviz')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='paula01',
     maintainer_email='paula01@todo.todo',
-    description='Robot diferencial URDF Xacro',
-    license='Apache-2.0',
+    description='TODO: Package description',
+    license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [],
+        'console_scripts': [
+        ],
     },
 )
